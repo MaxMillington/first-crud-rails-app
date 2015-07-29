3e70f36cf07521357ee136dc3b291bd83a5c4a42
MaxMillington <maxmillington89@gmail.com>
2015-07-29 13:42:54 -0600

    added authorization for an admin but not sure what to do with it yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :songs
   resources :users
 
+  namespace :admin do
+    resources :categories
+  end
+
   get "/logout", to: "sessions#destroy"
 
   get "/login", to: "sessions#new"
